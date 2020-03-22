3ad48c3280c003272c6142fd84e3618e03776062
kellycoles <kellycoles1@gmail.com>
2020-03-22 11:03:30 -0500

    configured identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GitFit.Models;
 
 namespace GitFit.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace GitFit.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+    }
+}
