f408ad0e959f5962c1b60bc0d8ec15ee36629573
kellycoles <kellycoles1@gmail.com>
2020-03-24 20:46:46 -0500

    activity model

---
+++ Models/Activity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitFit.Models
+{
+    public class Activity
+    {
+        [Key]
+        public int activityId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+
+        [Display(Name = "Duration")]
+        public int duration { get; set; }
+
+        [Display(Name = "Intensity")]
+        public string intensity { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
