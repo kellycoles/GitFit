322ee5adb8b923958e0422dea569558bf380f4e6
kellycoles <kellycoles1@gmail.com>
2020-03-24 20:39:05 -0500

    entry model

---
+++ Models/Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitFit.Models
+{
+    public class Entry
+    {
+        [Key]
+        public int entryId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+
+        public DateTime date { get; set; }
+
+        public string notes { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
